c89a5a567d71c45542003160c639663c43d0a178
Rohit <rohitj559@gmail.com>
2018-11-28 20:08:52 -0600

    Arrays implementation

---
+++ arrays.py
@@ -0,0 +1,30 @@
+numbers = [1, 2, 3, 100, 4, 5, 6]
+#print(numbers)
+
+#print(numbers[4])
+
+#for num in numbers:
+	#print(num)
+
+#numbers[2] = "rohit"
+	
+#for i in range(len(numbers)):
+	#print(numbers[i])
+	
+#print(numbers[0:2])
+#print(numbers[:-1])
+#print(numbers[:-2])
+#print(numbers[-1:])
+#print(numbers[-2:])
+
+#maximum = numbers[0]
+#for num in numbers:
+	#if num > maximum:
+		#maximum = num
+#print(maximum)
+
+
+
+	
+
+
