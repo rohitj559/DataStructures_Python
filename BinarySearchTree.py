f5547c18c366350f0d35a84aae4d4c57e02b0f02
Rohit Jagannath <rohitj559@gmail.com>
2018-12-04 17:58:14 -0600

    Create BinarySearchTree.py

---
+++ BinarySearchTree.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  4 17:55:52 2018
+
+@author: Rohit
+"""
+
+class Node(object):
+    
+    def __init__(self, data):
+        self.data = data;
+        self.leftChild = None;
+        self.rightChild = None;
+        
