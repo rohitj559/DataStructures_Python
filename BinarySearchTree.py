f95b809d03290c3154a05ffe98a11b579ae95c96
Rohit Jagannath <rohitj559@gmail.com>
2018-12-04 20:50:18 -0600

    Trverse in order, get the max and min in BST

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -17,16 +17,14 @@ class BinarySearchTree(object):
     def __init__(self):
         self.rootNode = None;
         
-    def Insert(self, data):
-        
+    def Insert(self, data):        
         if not self.rootNode:
             self.rootNode = Node(data);
         else:
             self.InsertNode(data, self.rootNode);
             
     # O(logN) if tree is balanced. Else, O(N)
-    def InsertNode(self, data, node):
-        
+    def InsertNode(self, data, node):        
         if data < node.data:
             if node.leftChild:
                 self.InsertNode(data, node.leftChild);
@@ -38,7 +36,32 @@ class BinarySearchTree(object):
             else:
                 node.rightChild = Node(data); 
                 
+    # Traverse in order, get the max and min in BST
+    # getting the min value
+    def GetMin(self):
+        if self.rootNode:
+            self.GetMinValue(self.rootNode);
+            
+    def GetMinValue(self, node):
+        if node.leftChild:
+            self.GetMinValue(node.leftChild);
+        else:
+            return node.data;
     
+    # getting the max value
+    def GetMax(self):
+        if self.rootNode:
+            self.GetMaxValue(self.rootNode);
+            
+    def GetMaxValue(self, node):
+        if node.rightChild:
+            self.GetMaxValue(node.rightChild);
+        else:
+            return node.data;
+        
+    # in-order traversal
+    
+            
                 
                 
             
